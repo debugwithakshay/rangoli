cec271d41fb519d8c6fb4e94f6b3fae652c711e5
Akshay <akshayyadavindia31@gmail.com>
2024-01-20 14:47:30 +0530

    fix numbering

---
+++ script.js
@@ -0,0 +1,12 @@
+let checkBox = document.querySelector(".checkBox");
+let iconCheck = document.querySelector(".checkBox .icon--check");
+
+checkBox.addEventListener("click", function () {
+  if (checkBox.classList.contains("checked")) {
+    checkBox.classList.replace("checked", "unchecked");
+    iconCheck.style.display = "none"
+  } else if (checkBox.classList.contains("unchecked")) {
+    checkBox.classList.replace("unchecked", "checked");
+    iconCheck.style.display = "inline"
+  }
+});
